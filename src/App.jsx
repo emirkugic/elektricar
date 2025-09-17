6c30fe73fb3cc358161e12fdb3df0de247ec36b4
emirkugic <emir.kugic@hotmail.com>
2025-09-17 15:13:40 +0200

    DONE v2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 			<div className="App">
 				<main>
 					<Routes>
-						<Route path="/" element={<Homepage />} />
+						<Route path="*" element={<Homepage />} />
 					</Routes>
 				</main>
 			</div>
